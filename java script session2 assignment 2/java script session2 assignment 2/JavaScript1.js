ad3161970bdd51bc16e8cbd51fbd46f4deda3107
praveenearle <praveen.earle@gmail.com>
2016-10-18 09:47:04 +0530

    java script s1a2
    java script s1a2

---
+++ java script session2 assignment 2/java script session2 assignment 2/JavaScript1.js
@@ -0,0 +1,35 @@
+﻿function numbers() {
+    var displayVal = '';
+    var finalval= 'false';
+    var secondval=false;
+    for (i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            finalval= 'true';
+        } else {
+            finalval = 'false';
+        }
+           
+        if (i % 5 == 0) {
+            secondval = 'true';
+        } else {
+            secondval = 'false';
+        }
+            
+        if (finalval === 'true' && secondval === 'true') {
+            displayVal += "FizzBuzz <br>";
+        }
+        else if (finalval === 'true') {
+            displayVal += "Fizz" + "<br>";
+           
+        }
+        else if (secondval === 'true') {
+            displayVal += "Buzz" + "<br>";
+            
+        } else {
+            displayVal += i + '<br>';
+        }
+        
+     
+    }
+    document.getElementById("virat").innerHTML = displayVal;
+}
